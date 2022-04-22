4085c8a36c7cfea6b34732f25ff6bdf95c5a28ff
Next <next@next-hyd-580.nexteducation.com>
2022-04-22 20:05:56 +0530

    fc

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var myApp = angular.module("myApp",[]);
+
+myApp.controller("myController",function($scope){
+	console.log("In myController...");
+	
+	
+	$scope.orders = [
+		{}
+		
+		
+		
+	]
+});
